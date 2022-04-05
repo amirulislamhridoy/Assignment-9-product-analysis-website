5d534bcde26565227b02f649c36525c6a3672d43
amirulislamhridoy <hossainhridoy30687@gmail.com>
2022-04-05 23:10:15 +0600

    conditional randering

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -3,9 +3,12 @@ import useReviews from '../../hook/useReview';
 
 const Reviews = () => {
     const [reviews, setReviews] = useReviews()
-    console.log(reviews)
+    // console.log(reviews)
     return (
         <div className='w-3/4 mx-auto my-10'>
+            {
+                reviews.length >= 6 ? <h1 className='text-3xl font-bold'>It's true because reviews are outside 6.</h1> : <h1 className='text-3xl font-bold'>It's false because reviews are inside 6</h1>
+            }
             {
                 reviews.map(review => <div key={review._id} className="my-6 font-sans flex items-center">
                     <img className='w-24 h-24' src={review.picture} alt="" />
